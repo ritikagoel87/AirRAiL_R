d3299e17643441e181711288b9e451fd65787947
Ritika Goel <ritika2654@gmail.com>
2019-05-08 11:56:05 +1000

    Added axios and started working on airplanes

--- src/components/Airplane.js
+++ src/components/Airplane.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+const SERVER_URL = 'http://localhost:3000/plane.json';
 
 class Airplane extends Component {
+  constructor() {
+    super();
+    this.state = { planes: [] };
+
+    const fetchPlanes = () => {
+      axios.get( SERVER_URL ).then( ( results) => {
+        this.setState({planes: results.data});
+      });
+    }
+
+  }
+
   render() {
     return (
       <div>
